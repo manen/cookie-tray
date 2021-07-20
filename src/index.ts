8f4958b3f2c9f742fd7b009079df1d059d05358a
manen <varga.bercel.i@gmail.com>
2021-07-20 06:01:34 +0200

    1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 export type Cookies = Record<string, string>;
 
 export function parse(str: string): Cookies {
+	if (!str) return {};
 	return Object.fromEntries(str.split('; ').map((s) => s.split('=')));
 }
 
 export function stringify(cookies: Cookies): string {
+	if (!cookies) return '';
 	return Object.entries(cookies)
 		.map((c) => c.join('='))
 		.join('; ');
